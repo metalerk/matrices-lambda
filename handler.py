7ea30c9b666cdbbfe1fc006d465b1c8f61f6d108
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-03 14:16:46 -0600

    pandas files

--- handler.py
+++ handler.py
@@ -2,12 +2,29 @@ import json
 import pandas as pd
 
 def get_line_number(path):
-    with open('matrices.csv', 'r') as f:
-        f.__next__()
-        for line in f.readlines():
-            print(line)
+	with open('matrices.csv', 'r') as f:
+		f.__next__()
+		f.__next__()
+		c = 2
+		for line in f.readlines():
+			c += 1
+			if not line[0].isdigit():
+				return c - 2
+	return 0
 
-def main(event, context):
-    matrices = pd.read_csv('matrices.csv')
-    print(matrices)
-    get_line_number('matrices.csv')
+# def main(event, context):
+def main():
+	dimension = get_line_number('matrices.csv')
+	matrix = pd.read_csv('matrices.csv')
+	initial_pos = 0
+	dimension_aux = dimension
+	list_df = list()
+	while(initial_pos < matrix.shape[0]):
+		# print(pd.DataFrame(matrix.iloc[initial_pos: dimension_aux].mean()))
+		list_df.append(matrix.iloc[initial_pos: dimension_aux].mean())
+		initial_pos += dimension
+		dimension_aux += dimension
+
+	return {}
+
+main()
