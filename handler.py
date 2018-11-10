3d1ebca26d5f1e9818628737f5fb55ff02d44ea0
Luis Esteban <rodriguezjluis0@gmail.com>
2018-11-10 11:45:52 -0600

    add args

--- handler.py
+++ handler.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 import json
 import pandas as pd
+import argparse
+
+from tabulate import tabulate
 
 def get_line_number(path):
 	with open('matrices.csv', 'r') as f:
@@ -14,8 +17,8 @@ def get_line_number(path):
 	return 0
 
 def main(*args, **kwargs):
-	dimension = get_line_number('matrices.csv')
-	matrix = pd.read_csv('matrices.csv')
+	dimension = get_line_number(kwargs['file'])
+	matrix = pd.read_csv(kwargs['file'])
 	initial_pos = 0
 	dimension_aux = dimension
 	list_df = list()
@@ -29,7 +32,31 @@ def main(*args, **kwargs):
 		[x.to_frame().T for x in list_df]
 	).reset_index(drop=True)
 
+	if kwargs['verbose']: print(tabulate(dfs, headers="keys", tablefmt="psql"))
+
+	if kwargs['output']:
+		if kwargs['mode'] == 'csv':
+			if kwargs['verbose']: print('[+] Escribiendo CSV...')
+			dfs.to_csv(kwargs['output'], sep=',')
+			if kwargs['verbose']: print('[+] Hecho.')
+
+		elif kwargs['mode'] == 'html':
+			if kwargs['verbose']: print('[+] Escribiendo HTML...')
+			dfs.to_html(kwargs['output'], escape=True)
+			if kwargs['verbose']: print('[+] Hecho.')
+
 	return {}
 
 if __name__ == '__main__':
-	main()
+	parser = argparse.ArgumentParser(description='Promedio de ancestría por población.')
+	parser.add_argument('-f', '--file', type=str, default='matrices.csv', help='Archivo CSV')
+	parser.add_argument('-o', '--output', type=str, help='Archivo resultante')
+	parser.add_argument('-m', '--mode', type=str, default='csv', help='Archivo HTML resultante')
+	parser.add_argument('-v', '--verbose', action='store_true', default=False, help='Verbose')
+
+	args = parser.parse_args()
+	main(file=args.file,
+		 output=args.output,
+		 mode=args.mode,
+		 verbose=args.verbose
+	)
